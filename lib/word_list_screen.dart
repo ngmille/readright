06ee16aea0e8df397e8a4575cc7cb704f947965f
Angileah <angileah10@gmail.com>
2025-10-27 20:56:31 -0400

    Set up of the screen navigation and main updated

---
+++ lib/word_list_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WordListScreen extends StatelessWidget {
+  const WordListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Login Screen (Coming soon)', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
